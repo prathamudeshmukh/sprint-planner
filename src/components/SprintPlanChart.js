bb9d0549244cd84be6ff763f29af16ddb70163a6
Administrator <prathamudeshmukh@gmail.com>
2019-07-12 11:17:40 +0530

    Added JSON for sprint data

--- src/components/SprintPlanChart.js
+++ src/components/SprintPlanChart.js
@@ -1,5 +1,6 @@
 import Chart from 'react-google-charts';
 import React from 'react';
+import sprintData from './sprint-data';
 
 export default class SprintPlanChart extends React.PureComponent {
     render() {
@@ -8,25 +9,7 @@ export default class SprintPlanChart extends React.PureComponent {
             height={'300px'}
             chartType="Timeline"
             loader={<div>Loading Chart</div>}
-            data={[
-                [
-                    { type: 'string', id: 'Stream' },
-                    { type: 'string', id: 'Story' },
-                    { type: 'date', id: 'Start' },
-                    { type: 'date', id: 'End' },
-                ],
-                ['Abhijit', 'DOP-1035',new Date(2019,6,11), new Date(2019,6,16) ],
-                ['Varun', 'DOP-1034',new Date(2019,6,11), new Date(2019,6,16) ],
-                ['Prathamesh', 'DOP-1038',new Date(2019,6,11), new Date(2019,6,25) ],
-                ['Prathamesh', 'DOP-1036',new Date(2019,6,15), new Date(2019,6,17) ],
-                ['Abhijit', 'DOP-1037',new Date(2019,6,17), new Date(2019,6,19) ],
-                ['Prathamesh', 'DOP-1029',new Date(2019,6,11), new Date(2019,6,12) ],
-                ['Anchal', 'DOP-1002',new Date(2019,6,11), new Date(2019,6,15) ],
-                ['Anchal', 'DOP-1000',new Date(2019,6,16), new Date(2019,6,19) ],
-                ['Varun', 'DOP-1022',new Date(2019,6,17), new Date(2019,6,19) ],
-                ['Prathamesh', 'DOP-1040',new Date(2019,6,17), new Date(2019,6,22) ],
-                ['Neha', 'DOP-1001',new Date(2019,6,11), new Date(2019,6,19) ],
-            ]}
+            data={sprintData}
             options={{
                 timeline: { groupByRowLabel: true },
                 hAxis: {
---
+++ src/components/sprint-data.js
@@ -0,0 +1,19 @@
+export default [
+                [
+                    { type: 'string', id: 'Stream' },
+                    { type: 'string', id: 'Story' },
+                    { type: 'date', id: 'Start' },
+                    { type: 'date', id: 'End' },
+                ],
+                ['Abhijit', 'DOP-1035',new Date(2019,6,11), new Date(2019,6,16) ],
+                ['Varun', 'DOP-1034',new Date(2019,6,11), new Date(2019,6,16) ],
+                ['Prathamesh', 'DOP-1038',new Date(2019,6,11), new Date(2019,6,25) ],
+                ['Prathamesh', 'DOP-1036',new Date(2019,6,15), new Date(2019,6,17) ],
+                ['Abhijit', 'DOP-1037',new Date(2019,6,17), new Date(2019,6,19) ],
+                ['Prathamesh', 'DOP-1029',new Date(2019,6,11), new Date(2019,6,12) ],
+                ['Anchal', 'DOP-1002',new Date(2019,6,11), new Date(2019,6,15) ],
+                ['Anchal', 'DOP-1000',new Date(2019,6,16), new Date(2019,6,19) ],
+                ['Varun', 'DOP-1022',new Date(2019,6,17), new Date(2019,6,19) ],
+                ['Prathamesh', 'DOP-1040',new Date(2019,6,17), new Date(2019,6,22) ],
+                ['Neha', 'DOP-1001',new Date(2019,6,11), new Date(2019,6,19) ],
+            ]
